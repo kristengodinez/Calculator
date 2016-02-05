f1e5217ee11c09a774ab2ba19a1b3ae2a03e8903
Kristen Godinez <kzgodinez@gmail.com>
2016-02-05 15:55:20 -0800

    second commit

---
+++ ContactEditor/src/my/contacteditor/CalculatorMain.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.contacteditor;
+
+import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+/**
+ *
+ * @authors Kristen Godinez, Winter Smith
+ */
+public class CalculatorMain {
+    public static void main(String[] args) {
+       JFrame frame = new ContactEditorUI();
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frame.setVisible(true);
+     }
+}
+
